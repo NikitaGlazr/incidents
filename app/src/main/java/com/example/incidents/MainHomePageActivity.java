581a04d5a9f3dfd14712c3d1240211352d1b1e2f
nikit <nglaz12@yandex.ru>
2024-05-27 13:56:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/incidents/MainHomePageActivity.java
@@ -0,0 +1,14 @@
+package com.example.incidents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainHomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_home_page);
+    }
+}
